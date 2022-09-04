dbee120ab9f3829b82a56757951f98a6f3cad6ff
maksbazunov <maksbazunov@yandex.ru>
2022-09-04 23:24:33 +0300

    fixed Bug brain-gcd

--- src/games/brain-gcd.js
+++ src/games/brain-gcd.js
@@ -7,18 +7,16 @@ const maxRandomNumber = 100;
 
 const maxDivider = (num1, num2) => {
   if (!num2) {
-        return num1;
-    }
-
-    return maxDivider(num2, num1 % num2);
+    return num1;
+  }
+  return maxDivider(num2, num1 % num2);
 };
 
 const genRound = () => {
-    const num1 = getRandomNum(minRandomNumber, maxRandomNumber);
-    const num2 = getRandomNum(minRandomNumber, maxRandomNumber);
-    const answer = String(maxDivider(num1, num2));
-    const question = `${num1} ${num2}` ;
-    
-    return [question, answer];
-  };
-  export default() => playGame(directiveGcd, genRound);
+  const num1 = getRandomNum(minRandomNumber, maxRandomNumber);
+  const num2 = getRandomNum(minRandomNumber, maxRandomNumber);
+  const answer = String(maxDivider(num1, num2));
+  const question = `${num1} ${num2}`;
+  return [question, answer];
+};
+export default () => playGame(directiveGcd, genRound);
