64db9fee93c3a50144b16e0112f3aee38c1227db
maksbazunov <maksbazunov@yandex.ru>
2022-09-08 19:51:40 +0300

    error correction brain-progression

--- src/games/brain-progression.js
+++ src/games/brain-progression.js
@@ -4,28 +4,28 @@ import playGame from '../index.js';
 const directiveProgress = 'What number is missing in the progression?';
 
 const genRound = () => {
-const randStart = getRandomNum(1,40);
-const randEnd = getRandomNum(60,100);
-const randStep = getRandomNum(2,6);
-let seriesNumArr = [];
+  const randStart = getRandomNum(1, 40);
+  const randEnd = getRandomNum(60, 100);
+  const randStep = getRandomNum(2, 6);
+  const seriesNumArr = [];
 
-for(let i = randStart; i <= randEnd; i+=1){
- seriesNumArr.push(i)
- i=i+randStep;
-if(seriesNumArr.length>14){
-    break
-}
+  for (let i = randStart; i <= randEnd; i += 1) {
+    seriesNumArr.push(i);
+    i += randStep;
+    if (seriesNumArr.length > 14) {
+      break;
+    }
+  }
+  const randomSeriesNum = seriesNumArr.length - (getRandomNum(1, seriesNumArr.length - 1));
+  const NumStr = seriesNumArr[randomSeriesNum];
+
+  const items = seriesNumArr;
+  const i = randomSeriesNum;
+  items[i] = '..';
+
+  const answer = String(NumStr);
+  const question = `${items.join()}`;
+  return [question, answer];
 };
-let randomSeriesNum = seriesNumArr.length-(getRandomNum(1,seriesNumArr.length-1))
-const NumStr = seriesNumArr[randomSeriesNum];
 
-const items = seriesNumArr;
-const i = randomSeriesNum
-items[i] = ".."
-
-    const answer = String(NumStr);
-    const question = `${items.join()}`;
-    return [question, answer];
-  };
-
-  export default () => playGame(directiveProgress, genRound);
+export default () => playGame(directiveProgress, genRound);
